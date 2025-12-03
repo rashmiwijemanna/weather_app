a254f97e3a4cb5fbe58be8983934e4796e82f632
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-12-03 20:59:34 +0530

    implement date format & addEvenListener functions

--- main.js
+++ main.js
@@ -11,5 +11,42 @@ const windOutput=document.querySelector('.wind');
 const form=document.getElementById('locationInput');
 const search=document.querySelector('.search');
 const btn=document.querySelector('.submit');
-const cities=document.querySelector('.cities');
+const cities=document.querySelector('.city');
+
+let cityInput= "London";
+
+cities.forEach((city) => {
+    cities.addEventListener('click', (e) => {
+        cityInput = e.target.innerHtml;
+
+        fetchWeatherData();
+        app.computedStyleMap.opacity = "0";
+    })
+    
+})
+
+form.addEventListener('submit', (e) => {
+    if(search.value.lenght==0){
+        alert('Please type in a city name');
+    }else{
+        cityInput=search.value;
+
+        fetchWeatherData();
+
+        search.value="";
+
+        app.style.opacity = 0;
+    }
+
+    e.preventDefault();
+})
+
+
+function dayOfTheWeek(day, month, year){
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    return weekday[new Date(`${day}/${month}/${year}`).getDay()];
+};
+
+
+
 
